1fab142a4d769f9555d9b309a7914ac17a7b2319
jamshidbekml <jamshidbekml@gmail.com>
2024-08-22 11:17:11 +0500

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SerialportModule } from './serialport/serialport.module';
 import { SmsModule } from './sms/sms.module';
+import { BullModule, InjectQueue } from '@nestjs/bull';
+import { ExpressAdapter } from '@bull-board/express';
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { Queue } from 'bull';
 
 @Module({
-  imports: [SerialportModule, SmsModule],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'sms',
+    }),
+    SerialportModule,
+    SmsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(@InjectQueue('sms') private readonly smsQueue: Queue) {
+    const serverAdapter = new ExpressAdapter();
+    serverAdapter.setBasePath('/admin/queues');
+    createBullBoard({
+      queues: [new BullAdapter(this.smsQueue)],
+      serverAdapter: serverAdapter,
+    });
+  }
+}
--- src/sms/sms.module.ts
+++ src/sms/sms.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
-import { BullModule } from '@nestjs/bull';
 import { SmsService } from './sms.service';
 import { SmsProcessor } from './sms.processor';
 import { SerialportModule } from 'src/serialport/serialport.module';
 import { SmsController } from './sms.controller';
 
 @Module({
-  imports: [
-    BullModule.forRoot({
-      redis: {
-        host: 'localhost',
-        port: 6379,
-      },
-    }),
-    BullModule.registerQueue({
-      name: 'sms',
-    }),
-    SerialportModule,
-  ],
+  imports: [SerialportModule],
   providers: [SmsProcessor, SmsService],
   exports: [SmsService],
   controllers: [SmsController],
